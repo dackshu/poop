98ffe0efcb898cf54998ce7615d14f7c97170b9f
Cayden30 <caydenharris354@gmail.com>
2026-07-22 00:58:31 -0400

    player.gd edit1
    edited player speed + jump height. added basic controls: "move_left: a",
    "move_right: d", and "jump: 'spacebar'".

--- cayden/player.gd
+++ cayden/player.gd
@@ -1,9 +1,11 @@
 extends CharacterBody2D
 
 
-const SPEED = 130.0
-const JUMP_VELOCITY = -250.0
+const SPEED = 230.0
+const JUMP_VELOCITY = -300.0
 
+# Get the gravity from the ProjectSettings to be synced with the RigidBody nodes
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 @onready var animated_sprite = $AnimatedSprite2D
 
@@ -16,15 +18,15 @@ func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	#Get the input direction: -1, 0, 1
+	# Get the input direction: -1, 0, 1
 	var direction := Input.get_axis("move_left", "move_right")
-
-	#Flip the sprite
+	
+	# Flip the Sprite
 	if direction > 0:
 		animated_sprite.flip_h = false
 	elif direction < 0:
 		animated_sprite.flip_h = true
-
+	
 	if direction:
 		velocity.x = direction * SPEED
 	else:
